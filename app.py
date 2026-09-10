0bb6f1110e88fea8095ed9aece3c99f8f5f22849
Olalemi Olaoluwakintan <133763138+Pro-phet123@users.noreply.github.com>
2026-09-10 04:14:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ from pathlib import Path
 
 st.set_page_config(
     page_title="Customer Value Intelligence",
-    page_icon="◈",
+    page_icon="⬢",
     layout="wide",
     initial_sidebar_state="expanded"
 )
